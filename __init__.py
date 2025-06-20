54829ebaedb04055f9f768bd0add9a6dae3f384e
Umer Sami <umer.sami@gmail.com>
2025-06-20 13:35:03 -0400

    Simplify shared package __init__.py to avoid circular imports

--- __init__.py
+++ __init__.py
@@ -10,25 +10,3 @@ This package provides common functionality for WeKare microservices:
 
 __version__ = "1.0.0"
 __author__ = "WeKare Team"
-
-# Import key modules for easy access
-from . import auth
-from . import config
-from . import database
-
-# Expose commonly used classes
-from .config.base_config import SharedInfrastructureConfig
-from .database.connection import get_database_url, get_session, db
-from .database.base import Base
-
-__all__ = [
-    "auth",
-    "config", 
-    "database",
-    "SharedInfrastructureConfig",
-    "get_database_url",
-    "get_session",
-    "db",
-    "Base",
-    "__version__",
-]
